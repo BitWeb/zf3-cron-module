9c8c41ddd812210447c9d5d5bc422501b36c70b3
tobre <tobre@bitweb.ee>
2016-11-16 11:38:41 +0200

    Factory fix

--- src/BitWeb/CronModule/Factory/CronServiceFactory.php
+++ src/BitWeb/CronModule/Factory/CronServiceFactory.php
@@ -4,21 +4,16 @@ namespace BitWeb\CronModule\Factory;
 
 use BitWeb\CronModule\Configuration;
 use BitWeb\CronModule\Service\CronService;
-use Zend\ServiceManager\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
 
 class CronServiceFactory implements FactoryInterface
 {
 
-    /**
-     * Create service
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return mixed
-     */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container,
+                             $requestedName, array $options = null)
     {
-        $config = $serviceLocator->get('Config');
+        $config = $container->get('Config');
         $config = $config['cronModule'];
 
         return new CronService(new Configuration($config));

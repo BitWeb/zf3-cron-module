47d506fa2fce33d1aa65edd6108eac1ba315feca
tobre <tobre@bitweb.ee>
2016-11-16 12:20:12 +0200

    Factory fix

--- config/controllers.config.php
+++ config/controllers.config.php
@@ -5,7 +5,7 @@ return [
         'BitWeb\CronModule\Controller\Index' => 'BitWeb\CronModule\Controller\IndexController'
     ],
     'initializers' => [
-        function ($controller, \Zend\Mvc\Controller\ControllerManager $cm) {
+        function (\Zend\ServiceManager\ServiceManager $cm, $controller) {
             if ($controller instanceof \BitWeb\CronModule\Service\Cron\CronServiceAwareInterface) {
                 $controller->setCronService($cm->get('BitWeb\CronModule\Service\Cron'));
             }
--- src/BitWeb/CronModule/Module.php
+++ src/BitWeb/CronModule/Module.php
@@ -4,6 +4,7 @@ namespace BitWeb\CronModule;
 
 use Zend\Console\Adapter\AdapterInterface;
 use Zend\ModuleManager\Feature\ControllerProviderInterface;
+use Zend\ServiceManager\ServiceManager;
 
 class Module implements ControllerProviderInterface
 {
@@ -19,7 +20,7 @@ class Module implements ControllerProviderInterface
                 'BitWeb\CronModule\Controller\Index' => 'BitWeb\CronModule\Controller\IndexController'
             ],
             'initializers' => [
-                function (\Zend\Mvc\Controller\ControllerManager $cm, $controller) {
+                function (ServiceManager $cm, $controller) {
                     if ($controller instanceof \BitWeb\CronModule\Service\Cron\CronServiceAwareInterface) {
                         $controller->setCronService($cm->get('BitWeb\CronModule\Service\Cron'));
                     }
